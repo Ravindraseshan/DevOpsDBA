fa04edb8f0a7aefff104f02f1c72a7b49a0e71fa
Ravin Yathati <ravindraseshan@gmail.com>
2025-04-16 20:58:47 -0400

    script

---
+++ sql/V1__createtable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE purchasing.productvendor (
+  productid INTEGER NOT NULL,
+  businessentityid INTEGER NOT NULL,
+  averageleadtime INTEGER NOT NULL,
+  standardprice DECIMAL(19, 4) NOT NULL,
+  lastreceiptcost DECIMAL(19, 4),
+  lastreceiptdate TIMESTAMP,
+  minorderqty INTEGER NOT NULL,
+  maxorderqty INTEGER NOT NULL,
+  onorderqty INTEGER,
+  unitmeasurecode CHAR(3) NOT NULL,
+  modifieddate TIMESTAMP NOT NULL DEFAULT localtimestamp
+);
